0e98e2bbf3c52a5dcbd91f5f5be425a1c5fbb825
Eben <me@ebenroux.co.za>
2016-03-27 10:12:31 +0200

    - v2.1.3

--- Shuttle.Esb.Scheduling/Properties/AssemblyInfo.cs
+++ Shuttle.Esb.Scheduling/Properties/AssemblyInfo.cs
@@ -25,10 +25,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Shuttle.Esb.Scheduling for .NET Framework 4.6.1")]
 #endif
 
-[assembly: AssemblyVersion("2.1.2.0")]
+[assembly: AssemblyVersion("2.1.3.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2010-2015")]
 [assembly: AssemblyProduct("Shuttle.Esb.Scheduling")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("2.1.2")]
+[assembly: AssemblyInformationalVersion("2.1.3")]
 [assembly: ComVisible(false)]
